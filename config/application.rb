157da18b9ba945711939149d64d316555f9d7b86
Svetlana Saveleva <ssaweleva@gmail.com>
2017-01-28 19:37:10 +0400

    use factories instead of fixtures in rails generator

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,12 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Task
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false, fixture_replacement: :factory_girl
+    end
   end
 end
