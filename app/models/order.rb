8f73e7a98803bc936762809133c3b7efc403bc81
Svetlana Saveleva <ssaweleva@gmail.com>
2017-01-28 15:38:47 +0400

    add state machine to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,27 @@
 class Order < ApplicationRecord
+  include AASM
+
+  aasm column: :state do
+    state :need_checking
+    state :unassigned, initial: true
+    state :assigned
+    state :performed
+    state :overdue
+
+    event :check do
+      transitions from: :need_checking, to: :unassigned
+    end
+
+    event :assigne do
+      transitions from: :unassigned, to: :assigned
+    end
+
+    event :perform do
+      transitions from: :assigned, to: :performed
+    end
+
+    event :delay do
+      transitions from: :unassigned, to: :overdue
+    end
+  end
 end
